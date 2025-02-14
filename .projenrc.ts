ebdd4469d5d89843d7a509bd7072bf2a0f8fb8f2
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2025-02-14 09:44:09 -0800

    cleanup and bump cdkVersion

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,22 +6,18 @@ const project = new clickupCdk.ClickUpCdkConstructLibrary({
   name,
   author: 'jose-clickup',
   authorAddress: 'jamoroso@clickup.com',
-  cdkVersion: '2.105.0',
+  cdkVersion: '2.178.2',
   defaultReleaseBranch: 'main',
   experimentalIntegRunner: true,
   gitignore: ['.vscode/**'],
   projenrcTs: true,
   packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: '9',
   repositoryUrl: `https://github.com/time-loop/${name}.git`,
 
   bundledDeps: ['@aws-sdk/client-cloudwatch', '@aws-sdk/client-service-quotas'],
   devDeps: ['@time-loop/clickup-projen', '@aws-cdk/integ-tests-alpha', 'aws-sdk-client-mock'],
   peerDeps: ['multi-convention-namer'],
 });
-
-// TODO remove aws-sdk v2 dependency.
-project.npmrc.addConfig('node-linker', 'hoisted'); // PNPM support for bundledDeps https://pnpm.io/npmrc#node-linker
 
 // Assume the usInfraDev role
 const build = project.tryFindObjectFile('.github/workflows/build.yml');
